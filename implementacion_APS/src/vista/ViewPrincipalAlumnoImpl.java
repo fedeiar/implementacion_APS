121cf524001f01c8d3625247dbca51f22e7f4acd
Federico <iarlorifederico@gmail.com>
2022-10-04 13:14:30 -0300

    gitignore

--- implementacion_APS/src/vista/ViewPrincipalAlumnoImpl.java
+++ implementacion_APS/src/vista/ViewPrincipalAlumnoImpl.java
@@ -1,16 +1,32 @@
 package vista;
 
+import java.awt.*;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Container;
+import javax.swing.SwingConstants;
+
+import controlador.ControllerLoginUsuario;
+import controlador.ControllerPrincipalAdministrador;
 
 
 public class ViewPrincipalAlumnoImpl extends JPanel implements ViewPrincipalAlumno{
     
 
     private MainWindow mainWindow;
-
-
+    private JLabel lblBienvenidoAlumno;
 
     public ViewPrincipalAlumnoImpl(MainWindow mainWindow){
         this.mainWindow = mainWindow;
@@ -23,7 +39,13 @@ public class ViewPrincipalAlumnoImpl extends JPanel implements ViewPrincipalAlum
     }
 
     private void inicializarComponentes(){
+        lblBienvenidoAlumno = new JLabel("Bienvenido Administrador.");
+        lblBienvenidoAlumno.setHorizontalAlignment(SwingConstants.CENTER);
+        lblBienvenidoAlumno.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        lblBienvenidoAlumno.setBounds(365, 36, 255, 60);
+        add(lblBienvenidoAlumno);
 
+        
     }
 
     private void inicializarListeners(){
